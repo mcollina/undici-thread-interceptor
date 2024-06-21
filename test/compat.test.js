2631698faaf956a566c9564936c15cb047e89b1c
Matteo Collina <hello@matteocollina.com>
2024-06-21 13:55:34 +0200

    split tests
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ test/compat.test.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const { test } = require('node:test')
+const { deepStrictEqual, strictEqual } = require('node:assert')
+const { join } = require('path')
+const { Worker } = require('worker_threads')
+const { createThreadInterceptor } = require('../')
+const { Agent, request } = require('undici')
+
+test('express', async (t) => {  
+  const worker = new Worker(join(__dirname, 'fixtures', 'express1.js'))
+  t.after(() => worker.terminate())
+
+  const interceptor = createThreadInterceptor({
+    domain: '.local'
+  })
+  interceptor.route('myserver', worker)
+
+  const agent = new Agent().compose(interceptor)
+
+  const { body, headers } = await request('http://myserver.local',{
+    dispatcher: agent
+  })
+
+  deepStrictEqual(await body.json(), { hello: 'world' })
+})
+
+test('koa', async (t) => {  
+  const worker = new Worker(join(__dirname, 'fixtures', 'koa1.js'))
+  t.after(() => worker.terminate())
+
+  const interceptor = createThreadInterceptor({
+    domain: '.local'
+  })
+  interceptor.route('myserver', worker)
+
+  const agent = new Agent().compose(interceptor)
+
+  const { body, headers } = await request('http://myserver.local',{
+    dispatcher: agent
+  })
+
+  deepStrictEqual(await body.json(), { hello: 'world' })
+})
+
