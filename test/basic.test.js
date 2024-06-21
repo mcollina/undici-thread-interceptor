de2ab053ccb3a9d5abbeeffcfd07e49cede201bb
Matteo Collina <hello@matteocollina.com>
2024-06-21 13:53:47 +0200

    Handle a worker terminated when another worker tries to call them
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,11 +1,13 @@
 'use strict'
 
 const { test } = require('node:test')
-const { deepStrictEqual } = require('node:assert')
+const { deepStrictEqual, strictEqual } = require('node:assert')
 const { join } = require('path')
 const { Worker } = require('worker_threads')
 const { createThreadInterceptor } = require('../')
 const { Agent, request } = require('undici')
+const { once } = require('events')
+const { setTimeout: sleep } = require('timers/promises')
 
 test('basic', async (t) => {  
   const worker = new Worker(join(__dirname, 'fixtures', 'worker1.js'))
@@ -18,10 +20,11 @@ test('basic', async (t) => {
 
   const agent = new Agent().compose(interceptor)
 
-  const { body, headers } = await request('http://myserver.local',{
+  const { statusCode, body, headers } = await request('http://myserver.local',{
     dispatcher: agent
   })
 
+  strictEqual(statusCode, 200)
   deepStrictEqual(await body.json(), { hello: 'world' })
 })
 
@@ -82,4 +85,67 @@ test('koa', async (t) => {
   })
 
   deepStrictEqual(await body.json(), { hello: 'world' })
+})
+
+test('two service in a mesh, one is terminated with an inflight message', async (t) => {  
+  const worker1 = new Worker(join(__dirname, 'fixtures', 'worker1.js'), {
+    workerData: { message: 'mesh' }
+  })
+  t.after(() => worker1.terminate())
+  const worker2 = new Worker(join(__dirname, 'fixtures', 'worker2.js'))
+  t.after(() => worker2.terminate())
+
+  const interceptor = createThreadInterceptor({
+    domain: '.local'
+  })
+  interceptor.route('myserver', worker1)
+  interceptor.route('myserver2', worker2)
+
+  const agent = new Agent().compose(interceptor)
+
+  worker1.terminate()
+
+  const res = await request('http://myserver2.local',{
+    dispatcher: agent
+  })
+
+  strictEqual(res.statusCode, 500)
+  deepStrictEqual(await res.body.json(), {
+    error: 'Internal Server Error',
+    message: 'Worker exited',
+    statusCode: 500
+  })
+})
+
+test('two service in a mesh, one is terminated, then a message is sent', async (t) => {  
+  const worker1 = new Worker(join(__dirname, 'fixtures', 'worker1.js'), {
+    workerData: { message: 'mesh' }
+  })
+  t.after(() => worker1.terminate())
+  const worker2 = new Worker(join(__dirname, 'fixtures', 'worker2.js'))
+  t.after(() => worker2.terminate())
+
+  const interceptor = createThreadInterceptor({
+    domain: '.local'
+  })
+  interceptor.route('myserver', worker1)
+  interceptor.route('myserver2', worker2)
+
+  const agent = new Agent().compose(interceptor)
+
+  worker1.terminate()
+
+  await once(worker1, 'exit')
+  await sleep(1000)
+
+  const res = await request('http://myserver2.local',{
+    dispatcher: agent
+  })
+
+  strictEqual(res.statusCode, 500)
+  deepStrictEqual(await res.body.json(), {
+    error: 'Internal Server Error',
+    message: `No server found for myserver.local in ${worker2.threadId}`,
+    statusCode: 500
+  })
 })
--- test/round-robin.test.js
+++ test/round-robin.test.js
@@ -1,11 +1,12 @@
 'use strict'
 
 const { test } = require('node:test')
-const { deepStrictEqual } = require('node:assert')
+const { deepStrictEqual, rejects } = require('node:assert')
 const { join } = require('path')
 const { Worker } = require('worker_threads')
 const { createThreadInterceptor } = require('../')
 const { Agent, request } = require('undici')
+const { once } = require('events')
 
 test('round-robin .route with array', async (t) => {  
   const worker1 = new Worker(join(__dirname, 'fixtures', 'worker1.js'), {
@@ -70,4 +71,73 @@ test('round-robin multiple .route', async (t) => {
 
     deepStrictEqual(await body.json(), { threadId: worker2.threadId })
   }
+})
+
+test('round-robin one worker exits', async (t) => {  
+  const worker1 = new Worker(join(__dirname, 'fixtures', 'worker1.js'), {
+    workerData: { message: 'mesh' }
+  })
+  t.after(() => worker1.terminate())
+  const worker2 = new Worker(join(__dirname, 'fixtures', 'worker1.js'))
+  t.after(() => worker2.terminate())
+
+  const interceptor = createThreadInterceptor({
+    domain: '.local'
+  })
+  interceptor.route('myserver', worker1)
+  interceptor.route('myserver', worker2)
+
+  const agent = new Agent().compose(interceptor)
+
+  {
+    const { body, headers } = await request('http://myserver.local/whoami',{
+      dispatcher: agent
+    })
+
+    deepStrictEqual(await body.json(), { threadId: worker1.threadId })
+  }
+
+  worker2.terminate()
+  // We must wait for the worker to exit
+  // otherwise the next request will 
+  await once(worker2, 'exit')
+
+  {
+    const { body, headers } = await request('http://myserver.local/whoami',{
+      dispatcher: agent
+    })
+
+    deepStrictEqual(await body.json(), { threadId: worker1.threadId })
+  }
+})
+
+test('round-robin one worker exits, in flight request', async (t) => {  
+  const worker1 = new Worker(join(__dirname, 'fixtures', 'worker1.js'), {
+    workerData: { message: 'mesh' }
+  })
+  t.after(() => worker1.terminate())
+  const worker2 = new Worker(join(__dirname, 'fixtures', 'worker1.js'))
+  t.after(() => worker2.terminate())
+
+  const interceptor = createThreadInterceptor({
+    domain: '.local',
+  })
+  interceptor.route('myserver', worker1)
+  interceptor.route('myserver', worker2)
+
+  const agent = new Agent().compose(interceptor)
+
+  {
+    const { body, headers } = await request('http://myserver.local/whoami',{
+      dispatcher: agent
+    })
+
+    deepStrictEqual(await body.json(), { threadId: worker1.threadId })
+  }
+
+  worker2.terminate()
+
+  await rejects(request('http://myserver.local/whoami',{
+    dispatcher: agent
+  }))
 })
