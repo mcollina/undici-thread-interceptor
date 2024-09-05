448081a39c9a0e2059599bd26d43e8fa5a0520ae
Paolo Insogna <paolo@cowtech.it>
2024-09-05 17:52:48 +0200

    fix: Correctly handle boolean option.

--- test/basic.test.js
+++ test/basic.test.js
@@ -337,3 +337,22 @@ test('timeout', async (t) => {
   empty.postMessage('close')
   await once(empty, 'exit')
 })
+
+test('timeout set to a boolean', async (t) => {
+  const empty = new Worker(join(__dirname, 'fixtures', 'empty.js'))
+
+  const interceptor = createThreadInterceptor({
+    domain: '.local',
+    timeout: true,
+  })
+  interceptor.route('myserver', empty)
+
+  const agent = new Agent().compose(interceptor)
+
+  await rejects(request('http://myserver.local', {
+    dispatcher: agent,
+  }), new Error('Timeout while waiting from a response from myserver.local'))
+
+  empty.postMessage('close')
+  await once(empty, 'exit')
+})
